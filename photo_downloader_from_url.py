31bfa9779353fc292b19285366cb7103a6596971
shadyskies <mehta.naman328@gmail.com>
2021-04-19 02:00:06 +0530

    image downloader

---
+++ photo_downloader_from_url.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import io
+import hashlib
+import requests
+import os
+
+
+# def download_images(folder_path, url, query, count):
+def download_images(url, folder_path):
+    try:
+        image_content = requests.get(url).content
+
+    except Exception as e:
+        print(f"ERROR - Could not download {url} - {e}")
+
+    try:
+        image_file = io.BytesIO(image_content)
+        image = Image.open(image_file).convert('RGB')
+        # folder_path = os.path.join(folder_path)
+        if os.path.exists(folder_path):
+            file_path = os.path.join(folder_path,hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
+        else:
+            os.mkdir(folder_path)
+            file_path = os.path.join(folder_path,hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
+        with open(file_path, 'wb') as f:
+            image.save(f, "JPEG", quality=85)
+        print(f"SUCCESS - saved {url} - as {file_path}")
+    except Exception as e:
+        print(f"ERROR - Could not save {url} - {e}")
+
+url = input("Enter url: ")
+download_images(url=url, folder_path="pics1")
